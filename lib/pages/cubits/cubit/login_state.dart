a7baa0d7d7b8ef38bf2136d35132bf0f3f300069
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2025-12-09 13:34:11 +0200

    Add LoginCubit and LoginState classes for authentication management

---
+++ lib/pages/cubits/cubit/login_state.dart
@@ -0,0 +1,12 @@
+part of 'login_cubit.dart';
+
+@immutable
+sealed class LoginState {}
+
+class LoginInitial extends LoginState {}
+
+class LoginLoading extends LoginState {}
+
+class LoginSuccess extends LoginState {}
+
+class Loginailure extends LoginState {}

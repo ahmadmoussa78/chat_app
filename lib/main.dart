911371a2e0da50aa9f6b75129e1a4a66e9bd570f
Ahmad Moussa <ahmadalhajmoussa78@gmail.com>
2025-12-15 14:52:05 +0200

    Added BlocObserver

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:chat_app/pages/chat_page.dart';
 import 'package:chat_app/pages/cubits/chat_cubit/chat_cubit.dart';
 import 'package:chat_app/pages/login_page.dart';
 import 'package:chat_app/pages/register_page.dart';
+import 'package:chat_app/simple_bloc_observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -11,7 +12,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const ChatApp());
+  Bloc.observer = SimpleBlocObserver();
 }
 
 class ChatApp extends StatelessWidget {
